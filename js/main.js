ae441d28040964aead8c33c2c2dfb9a07c917218
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-02 19:06:37 +0200

    Shell header animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
     $('select').material_select();
     $('#email_text').trigger('autoresize');
+
+    $("#info_link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#info").offset().top
+        }, 1000);
+    });
+
+    $("#projects_link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000);
+
+        console.log("clicked");
+    });
+
+    $("#skills_link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 1000);
+    });
+
+    $("#contact_link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
 })
